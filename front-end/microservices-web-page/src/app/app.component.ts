aeb9ab2303d2befc33591c01e33bc2f982dfc554
diogo <diogo.boaventura.ufms@gmail.com>
2025-02-24 12:07:02 -0300

    primeiro build backend e front end

---
+++ front-end/microservices-web-page/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { IndexComponent } from './pages/index/index.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [IndexComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'microservices-web-page';
+}
